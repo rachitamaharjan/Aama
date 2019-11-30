79736c749fb744af51b8b0ffe812cc7f53e359bf
Your Name <you@example.com>
2019-11-30 15:31:18 +0545

    background and forum

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+})
+
+/* GET forum page. */
+router.get('/forum', function (req, res, next) {
+  res.render('forum');
 });
-
-
+router.get('/singleblog', function (req, res, next) {
+  res.render('singleblog');
+});
 
 module.exports = router;
